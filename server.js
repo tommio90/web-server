e16156a07f791b873bb4b97246ef1b8d2299c5b1
Giuseppe Tomasello <gtomasello90@gmail.com>
2016-10-11 11:34:26 +0800

    Initialize repo

---
+++ server.js
@@ -0,0 +1,29 @@
+var express =require('express');
+var app = express();
+var PORT = 3000;
+
+var middleWare = {
+    requireAuthentication: function(req, res, next){
+        console.log('private route hit');
+        next();
+    },
+    logger: function (req, res, next){
+        console.log('Request : '+ req.method + ' '+ new Date().toString() +' '+ req.originalUrl);
+        next();
+    }
+};
+
+app.use(middleWare.logger);
+//app.use(middleWare.requireAuthentication);
+
+app.get('/about', middleWare.requireAuthentication, function(req, res){
+    res.send('about us!');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.listen(PORT, function(){
+    console.log('express started and running on port:  '+PORT+ '!');
+});
